fb26b184c7dc8f514e8036679eeef8fac58e7440
RudraMulchandani <85098459+RudraMulchandani@users.noreply.github.com>
2021-09-08 16:03:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+var engine,world;
+
+var ball;
+var blower;
+var blowerMouth;
+var button;
+
+
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create();
+  world=engine.world;
+
+  ball=new Ball(width/2+80,height/2-80,80,80);
+
+  blower=new Blower(width/2-50,height/2+50,150,20);
+
+  blowerMouth=new BlowerMouth(width/2+70,height/2+20,100,90);
+
+  button=createButton("click to blow");
+  button.position(width/2,height-100);
+  button.class("blowButton");
+  button.mousePressed(blow);
+}
+
+function draw() {
+  background(59); 
+  Engine.update(engine);
+
+  ball.show();
+  blower.show();
+  blowerMouth.show();
+}
+
+function blow(){
+  Matter.body.applyForce(ball.body,{x:0,y:0},{x:0,y:0.05});
+}
